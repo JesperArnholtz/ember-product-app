47aff1c52a3676983b988d4a528d9b75e68f321d
pentia-drummerboy <jna@pentia.dk>
2017-02-24 14:35:18 +0100

    adding admin pages

---
+++ app/routes/admin/categories.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model(){
+    return [
+          {
+            id: 1,
+            name: 'First Category'
+          },
+          {
+            id: 2,
+            name: 'Second Category'
+          }
+        ];
+  },
+
+  actions:{
+    addNewCategory(id, name){
+      this.controller.get('model').pushObject({id, name});
+    },
+
+    deleteCategory(category){
+      this.controller.get('model').removeObject(category);
+    }
+  }
+});
